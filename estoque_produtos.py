9690e0a21562bad8697c4602bfe276b90b3b4115
Luan Leone <luanleone05@gmail.com>
2021-04-13 21:03:09 -0300

    Created crossover function

--- estoque_produtos.py
+++ estoque_produtos.py
@@ -19,6 +19,7 @@ class Individuo():
         self.valores = valores
         self.limite_espacos = limite_espacos
         self.nota_avaliacao = 0
+        self.espaco_usado = 0
         self.geracao = geracao
         self.cromossomo = []
         
@@ -27,7 +28,34 @@ class Individuo():
                 self.cromossomo.append("0")
             else: 
                 self.cromossomo.append("1")
-                
+        
+    def avaliacao(self):
+        nota = 0
+        soma_espacos = 0
+        for i in range(len(self.cromossomo)):
+            if self.cromossomo[i] == '1':
+                nota += self.valores[i]
+                soma_espacos += self.espacos[i]
+            if soma_espacos > self.limite_espacos:
+                nota = 1
+            self.nota_avaliacao = nota
+            self.espaco_usado = soma_espacos
+            
+    def crossover(self, outro_individuo):
+        corte = round(random() * len(self.cromossomo))
+        
+        filho1 = outro_individuo.cromossomo[0:corte] + self.cromossomo[corte::]
+        filho2 = self.cromossomo[0:corte] + outro_individuo.cromossomo[corte::] 
+
+        filhos = [Individuo(self.espacos, self.valores, self.limite_espacos, self.geracao + 1),
+                  Individuo(self.espacos, self.valores, self.limite_espacos, self.geracao + 1)]
+        
+        filhos[0].cromossomo = filho1
+        filhos[1].cromossomo = filho2
+        
+        return filhos
+
+               
 if __name__ == '__main__':
     lista_produtos = []
     lista_produtos.append(Produto("Geladeira Dako", 0.751 , 999.90))
@@ -44,5 +72,44 @@ if __name__ == '__main__':
     lista_produtos.append(Produto("Geladeira Consul", 0.870 , 1199.89))
     lista_produtos.append(Produto("Notebook Lenovo", 0.498 , 1999.90))
     lista_produtos.append(Produto("Notebook Asus", 0.527 , 3999.00))
+    # for produto in lista_produtos:
+    #     print(produto.nome)
+    
+    espacos = []
+    valores = []
+    nomes = []
+    
     for produto in lista_produtos:
-        print(produto.nome)
+        espacos.append(produto.espaco)
+        valores.append(produto.valor)
+        nomes.append(produto.nome)
+    limite = 3
+    
+    individuo1 = Individuo(espacos, valores, limite)
+    #print("Espaços = %s" % str(individuo1.espacos))
+    #print("Valores = %s" % str(individuo1.valores))
+    #print("Cromosso = %s" % str(individuo1.cromossomo))
+    
+    print("\nIndividuo 1")
+    print("Cromosso = %s" % str(individuo1.cromossomo))
+    for i in range(len(lista_produtos)):
+        if individuo1.cromossomo[i] == '1':
+            print("Nome: %s R$ %s " % (lista_produtos[i].nome, lista_produtos[i].valor))
+    
+    individuo1.avaliacao()
+    print("Nota = %s" % individuo1.nota_avaliacao)
+    print("Espaço usado = %s" % individuo1.espaco_usado)
+    
+    individuo2 = Individuo(espacos, valores, limite)
+    print("\nIndividuo 2")
+    print("Cromosso = %s" % str(individuo2.cromossomo))
+    for i in range(len(lista_produtos)):
+        if individuo2.cromossomo[i] == '1':
+            print("Nome: %s R$ %s " % (lista_produtos[i].nome, lista_produtos[i].valor))
+    
+    individuo2.avaliacao()
+    print("Nota = %s" % individuo2.nota_avaliacao)
+    print("Espaço usado = %s" % individuo2.espaco_usado)
+    
+    individuo1.crossover(individuo2)
+    
